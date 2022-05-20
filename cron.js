119424a38500a86b340ff287d2a9fbfb249d3ecd
Bogdan Cazangiu <bogdan.cazangiu@gmail.com>
2022-05-20 22:34:46 +0300

    MSSQL import

--- cron.js
+++ cron.js
@@ -1,67 +1,72 @@
 const CRON = require('node-cron');
 const EXCEL = require('exceljs');
-const MYSQL = require('mysql');
 const MOMENT = require('moment');
+var MSSQL = require('mssql');
+const SqlString = require('tsqlstring');
 
 /** MySQL connection details */
-let connection = MYSQL.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
-});
-
-/** create connection to MySQL server */
-connection.connect(function (err) {
-    if (err) {
-        return console.error('error: ' + err.message);
-    }
-
-    console.log('Connected to the MySQL server.');
-});
+var config = {
+    "user": 'sa',
+    "password": 'Ericsson10UK!',
+    "server": 'localhost',
+    "database": 'stocks',
+    "dialect": "mssql",
+    'connectionTimeout': 50000,
+    "options": {
+        trustedconnection: true,
+        enableArithAbort: true,
+        encrypt: false,
+        useUTC: true,
+        instancename: 'MSSQLSERVER'  // SQL Server instance name
+    },
+};
 
 /** file to be parsed & loaded */
 const filename = "STOCK-EEC-17052022.xlsx";
 
 /** Schedule tasks to be run on the server. */
 CRON.schedule('* * * * *', function () {
-    console.log('running  task every minute, check README.md for cron config');
-    var workbook = new EXCEL.Workbook();
-    workbook.xlsx.readFile(filename)
-        .then(function () {
-            workbook.eachSheet((ws, sheetId) => {
-                var worksheet = workbook.getWorksheet(sheetId);
-                worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+    (async () => {
+        try {
+            // connect to your database
+            let pool = await MSSQL.connect(config);
+            // create Request object
+            const request = pool.request();
+            // query to the database and get the records
+            console.log('running  task every minute, check README.md for cron config');
+            var workbook = new EXCEL.Workbook();
+            workbook.xlsx.readFile(filename)
+                .then(function () {
+                    workbook.eachSheet((ws, sheetId) => {
+                        var worksheet = workbook.getWorksheet(sheetId);
+                        worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+                            if (rowNumber > 1) {
+                                /** add extra columns here */
+                                const rowWithExtra = row.values;
+                                rowWithExtra.push(MOMENT(new Date()).format('YYYY-MM-DD HH:mm:ss'));
+                                rowWithExtra.push(worksheet.name);
 
-                    if (rowNumber > 1) {
-                        /** add extra columns here */
-                        const rowWithExtra = row.values;
-                        rowWithExtra.push(MOMENT(new Date()).format('YYYY-MM-DD HH:mm:ss'));
-                        rowWithExtra.push(worksheet.name);
+                                /** indexing in xlsx files starts from number 1 (eg. A1 point to row 1 and column 1) */
+                                /** setting null at the start is much easier than always recalculating from 0 based to 1 based indexing */
+                                rowWithExtra.shift();
 
-                        /** escape strings */
-                        const rowValues = rowWithExtra.map(rowValue => {
-                            return connection.escape(rowValue);
-                        })
+                                /** prepare SQL to insert */
+                                var sql = SqlString.format("INSERT INTO [dbo].[stockecc] ([Reference],[Description],[Brand],[Stock],[Price],[CreatedDate],[BrandShort]) VALUES (?, ?, ?, ?, ?, ?, ?)", rowWithExtra);
 
-                        /** indexing in xlsx files starts from number 1 (eg. A1 point to row 1 and column 1) */
-                        /** setting null at the start is much easier than always recalculating from 0 based to 1 based indexing */
-                        rowValues.shift();
-
-                        /** join to creat the sql values */
-                        const rowValuesForSQL = rowValues.join(',');
-
-                        /** prepare SQL to insert */
-                        var sql = "INSERT INTO `stockeec` (`Reference`, `Description`, `Brand`, `Stock`, `Price`, `CreatedDate`, `BrandShort`) VALUES (" + rowValuesForSQL + ")";
-
-                        /** insert row */
-                        connection.query(sql, function (err, result) {
-                            if (err) throw err;
-                            //console.log("1 record inserted");
+                                /** insert row */
+                                request.query(sql, function (err, result) {
+                                    if (err) throw err;
+                                    console.log(result.rowsAffected);
+                                });
+                            }
                         });
-                    }
+                        console.log("Done sheet " + worksheet.name);
+                    })
                 });
-                console.log("Done sheet " + worksheet.name);
-            })
-        });
+        } catch (err) {
+            // ... error checks
+            console.log(err);
+            console.dir(err);
+        }
+    })()
 });
