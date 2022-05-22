ca6a6310b73fdf8f028828e2d61af1ed5a0f8f14
Bogdan Cazangiu <bogdan.cazangiu@gmail.com>
2022-05-22 15:08:03 +0300

    stockecc table script udpate

--- stockecc.sql
+++ stockecc.sql
@@ -1,7 +1,7 @@
 USE [stocks]
 GO
 
-/****** Object:  Table [dbo].[stockecc]    Script Date: 5/20/2022 10:36:38 PM ******/
+/****** Object:  Table [dbo].[stockecc]    Script Date: 5/22/2022 3:07:36 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,12 +9,12 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[stockecc](
-	[Reference] [nvarchar](50) NOT NULL,
+	[Reference] [nvarchar](50) NULL,
 	[Description] [nvarchar](255) NULL,
-	[Brand] [nvarchar](50) NOT NULL,
+	[Brand] [nvarchar](50) NULL,
 	[Stock] [int] NOT NULL,
 	[Price] [float] NOT NULL,
 	[CreatedDate] [datetime] NOT NULL,
-	[BrandShort] [nvarchar](50) NOT NULL
+	[BrandShort] [nvarchar](50) NULL
 ) ON [PRIMARY]
 GO
